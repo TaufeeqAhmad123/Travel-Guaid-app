a5296cfee77cca4f4492b28b390a54a048e240a7
TaufeeqAhmad123 <130512752+TaufeeqAhmad123@users.noreply.github.com>
2025-03-26 23:01:22 +0500

    initial commit
    adding assets and constant

---
+++ travel_guaid_app/lib/const/color.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const primaryColor = Colors.white;
+  static const secondaryColor = Color.fromARGB(255, 75, 68, 47);
+  static const tertiaryColor = Color(0xff010209);
+  static const darkSecondaryColor = Color(0xff080e2b);
+  static const lightTertiaryColor = Color(0xff040613);
+  static const white = Colors.white;
+  static const lightSecondary = Color.fromARGB(255, 7, 8, 13);
+}
